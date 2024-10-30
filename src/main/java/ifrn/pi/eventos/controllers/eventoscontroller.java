c94eb7af82d00331ebb8aa4f3b497dcc70e9b70e
jhonata <jhonata.paiva@escolar.ifrn.edu.br>
2024-10-30 17:14:54 -0300

    configura o banco de dados

--- src/main/java/ifrn/pi/eventos/controllers/eventoscontroller.java
+++ src/main/java/ifrn/pi/eventos/controllers/eventoscontroller.java
@@ -1,8 +1,13 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class eventoscontroller {
+public class EventosController {
 
+	@RequestMapping("/eventos/form")
+	public String form() {
+		return "formEventos";
+	}
 }
